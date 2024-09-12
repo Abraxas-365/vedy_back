07fa6b7dc7ab74c9dc3ef090e3f272c5b9cf3e95
Abraxas-365 <luisfmiranda8@gmail.com>
2024-09-12 18:46:48 -0500

    aaa

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use std::sync::Arc;
 
 use actix_cors::Cors;
 use actix_web::{middleware::Logger, web, App, HttpServer};
-use modules::{property, tenant};
+use modules::{
+    front::landing::{config, hero},
+    property, tenant,
+};
 use utils::s3;
 
 use crate::utils::database::PostgresRepository;
@@ -20,8 +23,10 @@ async fn main() -> std::io::Result<()> {
 
     let repo = Arc::new(PostgresRepository::new().await);
     let bukcet_service = Arc::new(s3::S3Repository::new().await.unwrap());
-    let property_service = Arc::new(property::Service::new(repo.clone(), bukcet_service));
+    let property_service = Arc::new(property::Service::new(repo.clone(), bukcet_service.clone()));
     let tenant_service = Arc::new(tenant::Service::new(repo.clone()));
+    let hero_service = Arc::new(hero::Service::new(repo.clone(), bukcet_service.clone()));
+    let config_service = Arc::new(hero::Service::new(repo.clone(), bukcet_service.clone()));
     let luci_service = Arc::new(utils::lucia::Service::new(repo.clone()));
 
     log::info!("Starting HTTP server on 0.0.0.0:80...");
@@ -32,9 +37,13 @@ async fn main() -> std::io::Result<()> {
             .wrap(cors)
             .wrap(Logger::default())
             .configure(property::config)
+            .configure(hero::config)
+            .configure(config::config)
             .app_data(web::Data::new(luci_service.clone()))
             .app_data(web::Data::new(property_service.clone()))
             .app_data(web::Data::new(tenant_service.clone()))
+            .app_data(web::Data::new(config_service.clone()))
+            .app_data(web::Data::new(hero_service.clone()))
     })
     .bind("0.0.0.0:3000")?
     .run()
