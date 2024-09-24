660adcfca07f3509dffff8a9d9d0d20c6d829ee7
Abraxas-365 <luisfmiranda8@gmail.com>
2024-09-24 16:15:19 -0500

    fixt routes

--- src/modules/front/landing/config/api/mod.rs
+++ src/modules/front/landing/config/api/mod.rs
@@ -7,7 +7,7 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/config")
             .route("", web::put().to(update_config))
-            .route("/{tenant_id}", web::get().to(get_tenant_config))
+            .route("/tenant/{tenant_id}", web::get().to(get_tenant_config))
             .route(
                 "/logo-upload-url",
                 web::get().to(generate_logo_presigned_url),
